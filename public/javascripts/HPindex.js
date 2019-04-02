1fa2aabcfdaf00684e097ea8554eb7f0a4966a2c
Mike Roth <mikeroth@Dales-MacBook-Pro-2.local>
2019-04-02 18:26:12 -0500

    Actual current version

--- public/javascripts/HPindex.js
+++ public/javascripts/HPindex.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById("avocado"), // Required
-  path: "avocado.json", // Required
+  path: "../images/avocado.json", // Required
   renderer: "svg", // Required
   loop: true, // Optional
   autoplay: true, // Optional
